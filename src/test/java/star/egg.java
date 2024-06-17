98a932ee2c19984cc87bba477b32150e71a6da5f
Snehaevs <snehashammykumarev@gmail.com>
2024-06-17 17:21:16 +0530

    First Commit of Star project

---
+++ src/test/java/star/egg.java
@@ -0,0 +1,16 @@
+package star;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class egg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/client");
+
+	}
+
+}
